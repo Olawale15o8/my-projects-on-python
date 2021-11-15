bb003628edabca70c82d91d0466e724734f7aee4
olawaleadisa <olawaleadisa@gmail.com>
2021-11-15 09:47:19 -0600

    my first git class

---
+++ introdgit.py
@@ -0,0 +1,7 @@
+greeting=("Hello goood morning how are u doing")
+# comand for using Git
+# git + init= to initialise our work
+# git + add= to check our work if it is add to git
+# git + status
+# git + commit
+# git + reset
